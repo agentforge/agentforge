80fb4f33e0c0505482dbdd2e4f97f76fb83a5201
Frank Grove <fragro@gmail.com>
2023-02-18 23:42:13 -0600

    Additional fixes

--- worker/inference/gpt_neo_chat.py
+++ worker/inference/gpt_neo_chat.py
@@ -95,14 +95,14 @@ class GPTChatbot:
     input_str = self.preprocess_input(input_str)
 
     # Update the conversation history
-    self.history.append(f"{self.name}: {input_str}")
+    self.history.append(f"{self.name}: {input_str} ")
 
     # Generate a response to the user input
     hist = self.history.relevant_history(self._c.config['history_cache_stack'])
-    print(f"actual input: {self.default_context.replace('#human', self.name) + ' ' + self.context + ' '.join(hist) + self.robot_name + ' :'}")
+    print(f"actual input: {self.default_context.replace('#human', self.name) + ' ' + self.context + ' '.join(hist) + self.robot_name + ': '}")
 
     # Use the GPT model to generate a response to the given prompt
-    prompt = self.default_context.replace("#human", self.name) + " " + self.context + " " + "\n".join(hist) + f" {self.robot_name}:"
+    prompt = self.default_context.replace("#human", self.name) + " " + self.context + " " + "\n".join(hist) + " " + f"{self.robot_name}: "
     response = self.generate_response(prompt)
 
     # Extract the generated text from the response
@@ -111,7 +111,7 @@ class GPTChatbot:
     # Use regular expressions to remove any leading or trailing whitespace
     generated_text = re.sub(r"^\s+|\s+$", "", generated_text)
 
-    total_context = self.default_context  + " " +  self.context
+    total_context = self.default_context.replace("#human", self.name)  + " " +  self.context
     new_phrase = self.history.find_new_phrase(generated_text, total_context, self.name)
 
     # Second pass for more content
