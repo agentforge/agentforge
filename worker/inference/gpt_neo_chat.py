0cd2969afca7c6badb3d759c20c03c74536781e8
Frank Grove <fragro@gmail.com>
2023-02-19 02:53:03 -0600

    Modify min max

--- worker/inference/gpt_neo_chat.py
+++ worker/inference/gpt_neo_chat.py
@@ -33,12 +33,12 @@ class GPTChatbot:
   def min_length(self, prompt):
     # Returns the optimal min_length for this model
     value = random.randint(self._c.config["min_length_itr_min"], self._c.config["min_length_itr_max"])
-    return len(prompt) + value
+    return int(max(len(prompt), value))
 
   def max_length(self, prompt):
     value = random.randint(self._c.config["max_length_itr_min"], self._c.config["max_length_itr_max"])
     # Returns the optimal max_length for this model
-    return len(prompt) + value
+    return int(max(len(prompt), value))
 
   def preprocess_input(self, input_str):
     # Use regular expressions to remove any leading or trailing whitespace
