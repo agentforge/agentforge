564c2b8e83503f8a08aac3e6b140ede3e4c1449c
Frank Grove <fragro@gmail.com>
2023-02-18 23:49:30 -0600

    Use the right quote

--- worker/inference/gpt_neo_chat.py
+++ worker/inference/gpt_neo_chat.py
@@ -101,7 +101,7 @@ class GPTChatbot:
 
     # Generate a response to the user input
     hist = self.history.relevant_history(self._c.config['history_cache_stack'])
-    print(f"actual input: {self.default_context + ' ' + self.context + ' '.join(hist) + " " + self.robot_name + ': '}")
+    print(f"actual input: {self.default_context + ' ' + self.context + ' '.join(hist) + ' ' + self.robot_name + ': '}")
 
     # Use the GPT model to generate a response to the given prompt
     prompt = self.default_context + " " + self.context + " " + "\n".join(hist) + " " + f"{self.robot_name}: "
