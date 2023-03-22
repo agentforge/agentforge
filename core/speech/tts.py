d7477dc7cb88e585834785f26df0a433f73e2194
Frank Grove <fragro@gmail.com>
2023-03-21 21:32:08 -0500

    Try speaker

--- core/speech/tts.py
+++ core/speech/tts.py
@@ -7,14 +7,18 @@ from TTS.api import TTS
 class TextToSpeech():
   def __init__(self) -> None:
     # Init TTS
-    self.tts = TTS(model_name="tts_models/en/ek1/tacotron2", progress_bar=False, gpu=True)
+    # self.tts = TTS(model_name="tts_models/en/ek1/tacotron2", progress_bar=False, gpu=True)
+    self.tts = TTS(model_name="tts_models/multilingual/multi-dataset/your_tts", progress_bar=False, gpu=True)
+
 
   def synthesizer(self, text, filename):
     # synthesis
     print(f"TTS: {text}")
     # with torch.no_grad():
     # start = time.time()
-    self.tts.tts_to_file(text=text, file_path=filename)
+    # self.tts.tts_to_file(text=text, file_path=filename)
+    self.tts.tts_to_file(text=text, file_path=filename, speaker_wav="/app/cache/voice.wav", language="en")
+
     # wav = self.text2speech(text)["wav"]
     # rtf = (time.time() - starts) / (len(wav) / self.text2speech.fs)
     # print(f"RTF = {rtf:5f}")
