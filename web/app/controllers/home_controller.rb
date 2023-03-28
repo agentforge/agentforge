25d9151f2e0232eed9dfc01ee641dde873cc24ad
Frank Grove <fragro@gmail.com>
2023-03-28 14:46:55 -0500

    Add FDR

--- web/app/controllers/home_controller.rb
+++ web/app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def index
     @model_configs = ["creative", "logical", "moderate"]
     @models = ["alpaca-lora-7b"]
-    @avatars = ["default", "makhno"]
+    @avatars = ["default", "makhno", "fdr"]
     render 'home/index'
   end
 end
