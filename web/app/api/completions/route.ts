ff0f272faf1f6ef66aad147e3aceb2fcaaef7090
Frank Grove <fragro@gmail.com>
2023-06-16 14:54:48 +0000

    Web updates and cleanup

--- web/app/api/completions/route.ts
+++ web/app/api/completions/route.ts
@@ -3,7 +3,6 @@ import { NextResponse } from 'next/server';
 import api_url from '@/components/shared/api';
 
 export async function POST(request: Request) {
-  console.log(request);
   const requestBody = await request.json();
   const res = await fetch(`${api_url}/v1/completions`, {
     method: 'POST',
@@ -13,8 +12,5 @@ export async function POST(request: Request) {
     },
     body: JSON.stringify(requestBody),
   });
-  console.log(res);
-  // const data = await res.json();
- 
   return res;
 }
