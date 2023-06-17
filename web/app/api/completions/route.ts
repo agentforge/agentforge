b3a15557fc05212eb5cde60b6cce8b88cb33b237
Frank Grove <fragro@gmail.com>
2023-06-17 23:51:46 +0000

    Updates to use new API key for completions

--- web/app/api/completions/route.ts
+++ web/app/api/completions/route.ts
@@ -8,7 +8,7 @@ export async function POST(request: Request) {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
-      // 'API-Key': process.env.DATA_API_KEY,
+      'X-API-KEY': process.env.DATA_API_KEY,
     },
     body: JSON.stringify(requestBody),
   });
