f8a136fa186ccb650e2affc1768754cc70abaccb
Frank Grove <fragro@gmail.com>
2023-07-09 22:03:30 +0000

    Speech upgrades

--- agentforge/api/tts.py
+++ agentforge/api/tts.py
@@ -4,34 +4,40 @@ from pydantic import BaseModel
 from agentforge.utils import measure_time, comprehensive_error_handler
 from agentforge.factories import resource_factory
 from dotenv import load_dotenv
-from .app import init_api
+from agentforge.api.app import init_api
+import traceback
 load_dotenv('../../../.env')
 
 app = init_api()
 tts = resource_factory.get_resource("tts")
+print("Speech API Ready")
 
 class TtsResponse(BaseModel):
   filename: str
 
 # Given the following text request generate a wav file and return to the client
 @app.post("/v1/tts", operation_id="createAudioResponse")
-@comprehensive_error_handler
-@measure_time
 async def text_to_speech(request: Request) -> TtsResponse:
   # Get the text and filename from the request
-  prompt = request.json["prompt"]
-  avatar = request.json["avatar"]
-
-  filename = "/app/cache/out.wav" # TODO: This is not scalable we need to establish a different filename per request
-  speaker_wav = os.environ.get('DST_PATH') + avatar["speaker_wav"] if "speaker_wav" in avatar else None
-  speaker_idx = avatar["speaker_idx"] if "speaker_idx" in avatar else 0
-
-  # Enqueue a job in the TTS pipeline
-  filename = tts.synthesizer(
-    prompt,
-    filename, 
-    speaker_wav=speaker_wav,
-    speaker_idx=speaker_idx
-  )
-  # Return the wav file in the response
-  return TtsResponse(filename=filename)
+  print("lfg")
+  try:
+    data = await request.json()
+    print(data)
+    prompt = data["response"]
+    avatar = data["avatar_config"]
+    filename = "/app/cache/out.wav" # TODO: This is not scalable we need to establish a different filename per request
+    speaker_wav = os.environ.get('DST_PATH') + avatar["speaker_wav"] if "speaker_wav" in avatar else None
+    speaker_idx = avatar["speaker_idx"] if "speaker_idx" in avatar else 0
+    print(prompt, filename, speaker_wav, speaker_idx)
+    # Enqueue a job in the TTS pipeline
+    filename = tts.synthesizer(
+      prompt,
+      filename,
+      speaker_wav=speaker_wav,
+      speaker_idx=speaker_idx
+    )
+    # Return the wav file in the response
+    return TtsResponse(filename=filename)
+  except Exception as e:
+    print(e)
+    print(traceback.format_exc())
--- docker/speech.Dockerfile
+++ docker/speech.Dockerfile
@@ -27,7 +27,7 @@ RUN pip install --upgrade pip
 
 WORKDIR /app/agentforge/agentforge/
 RUN mkdir /app/agentforge/logs/
-# RUN pip install -r /app/agentforge/requirements.txt
+RUN pip install --ignore-installed -r /app/agentforge/requirements.txt
 COPY .env /app/agentforge/.env
 
 # #CMD ["flask", "run", "--host=0.0.0.0", "--port=3000"]
